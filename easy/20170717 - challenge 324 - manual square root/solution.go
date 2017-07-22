58a0ac61d97a4a54ee73b4a48bdbf0941aa686f3
Tony Fortes Ramos <tony.fortesramos@ogd.nl>
2017-07-23 00:50:33 +0200

    Ugly go solution

---
+++ easy/20170717 - challenge 324 - manual square root/solution.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/big"
+	"strings"
+	"strconv"
+)
+
+/*
+See the section 'More accurate rough estimation' on the following page:
+http://www.hermes-press.com/square_root.htm
+
+Provided that n » 1
+
+1. Take the integer part of the number n. Z = int(n)
+2. Count the number of digits in Z. Let D be the number of digits.
+3. Calculate the value of 3.16 ^ D where the symbol " ^ " is to-the-n-power operator. Note: 3.16 ≈ √10
+4. The estimate is the value obtain in step 3 multiple by an adjustment factor based on the first (left most) digit of the number n. E = ADJ * 3.16 ^ D
+
+Table of ADJ
+
+First Digit	ADJ
+1 			0.32
+2			0.45
+3			0.55
+4			0.63
+5			0.71
+6			0.78
+7			0.84
+8			0.90
+9			0.95
+*/
+
+var adjCorrection = map[float64]float64 {
+	1: 0.32,
+	2: 0.45,
+	3: 0.55,
+	4: 0.63,
+	5: 0.71,
+	6: 0.78,
+	7: 0.84,
+	8: 0.90,
+	9: 0.95,
+}
+
+func digitsInInt (f float64) float64 {
+	return math.Floor(math.Log10(f)) + 1
+}
+
+func sqrEstimate (f float64) float64 {
+	var digitCount = digitsInInt(f)
+	div10Pow := f / (math.Pow(10, digitCount -1))
+	firstDigit := math.Floor(math.Abs(div10Pow))
+	return math.Pow(3.16, digitCount) * adjCorrection[firstDigit]
+}
+
+func sqrNewton (f float64, i float64) string {
+	var x float64
+	if f > 1 {
+		x = sqrEstimate(f)
+	} else {
+		x = 1
+	}
+	d := (x*x - f) / (2 * x)
+	for math.Abs(d) > math.Pow(10, -i) {
+		x = x - d
+		d = (x*x - f) / (2 * x)
+	}
+	return big.NewFloat(x).Text('f', int(i))
+}
+
+func main() {
+	challenge := []string{
+		"0 12345",
+		"8 123456",
+		"1 12345678901234567890123456789",
+	}
+	for _,s := range challenge {
+		v := strings.Split(s, " ")
+		var fv []float64
+		for _,i := range v {
+			j,_ := strconv.ParseFloat(i,64)
+			fv = append(fv,j)
+		}
+		fmt.Println(sqrNewton(fv[1],fv[0]))
+	}
+}
